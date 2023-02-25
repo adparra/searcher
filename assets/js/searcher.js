3ef6514a00ee2bf9a79c6ee54b966c75a1b2acbd
Cliente CONTROL-PC <CltControl@GGIECLUGWRK196.espol.edu.ec>
2023-02-25 11:39:16 -0500

    setup inicial de pagina searcher

---
+++ assets/js/searcher.js
@@ -0,0 +1,2 @@
+let urlJSON= 'https://raw.githubusercontent.com/Bootcamp-Espol/FSD02/main/S03D03/clase/recursos/products.json'
+let urlXML= 'https://raw.githubusercontent.com/Bootcamp-Espol/FSD02/main/S03D03/clase/recursos/products.xml'
