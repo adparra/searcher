ee4f1c2fd2215165446f9dc27d0f05d25a0d25a7
adparra <arturoparravalencia@gmail.com>
2023-03-03 22:21:33 -0500

    se agregó la carga de los elementos del archivo xml a la pagina

--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -1,5 +1,5 @@
-let urlJSON= 'https://raw.githubusercontent.com/Bootcamp-Espol/FSD02/main/S03D03/clase/recursos/products.json'
-let urlXML= 'https://raw.githubusercontent.com/Bootcamp-Espol/FSD02/main/S03D03/clase/recursos/products.xml'
+let urlJSON= 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json'
+let urlXML= 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml'
 
 let loadProducts= async (myURLJ,myURLX) =>{
     try{
@@ -9,41 +9,71 @@ let loadProducts= async (myURLJ,myURLX) =>{
         let resultsX= await responseX.text();
         let xml= (new DOMParser()).parseFromString(resultsX, 'application/xml');
         
-        for (const element of json){
+        cargarJSON(json);
+        cargarXML(xml);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+function cargarJSON(json) {
+  for (const element of json){
+    let message=`<div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
+      <div class="card card-blog card-plain">
+        <div class="card-header p-0 mt-n4 mx-3">
+          <a class="d-block shadow-xl border-radius-xl">
+            <img src="${element.src}" alt="${element.name}" class="img-fluid shadow border-radius-xl">
+          </a>
+        </div>
+        <div class="card-body p-3">
+          <p class="mb-0 text-sm">${element.type}</p>
+          <a href="javascript:;">
+            <h5>
+              ${element.name}
+            </h5>
+          </a>
+          <p class="mb-4 text-sm">
+            <b>Price: </b> $ ${element.price}
+          </p>
+        </div>
+      </div>
+    </div>`
+    let elementProduct= document.getElementsByClassName("row");
+    elementProduct[0].innerHTML+=message;
+  }
+}
+
+function cargarXML(xml) {
+  let arrProductos= xml.getElementsByTagName("product");
+        for (const element of arrProductos){
+            let namexml=element.getElementsByTagName("name")[0].innerHTML;
+            let srcxml=element.getElementsByTagName("src")[0].innerHTML;
+            let typexml=element.getElementsByTagName("type")[0].innerHTML;
+            let pricexml=element.getElementsByTagName("price")[0].innerHTML;
             let message=`<div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
               <div class="card card-blog card-plain">
                 <div class="card-header p-0 mt-n4 mx-3">
                   <a class="d-block shadow-xl border-radius-xl">
-                    <img src="${element.src}" alt="${element.name}" class="img-fluid shadow border-radius-xl">
+                    <img src="${srcxml}" alt="${namexml}" class="img-fluid shadow border-radius-xl">
                   </a>
                 </div>
                 <div class="card-body p-3">
-                  <p class="mb-0 text-sm">${element.type}</p>
+                  <p class="mb-0 text-sm">${typexml}</p>
                   <a href="javascript:;">
                     <h5>
-                      ${element.name}
+                      ${namexml}
                     </h5>
                   </a>
                   <p class="mb-4 text-sm">
-                    <b>Price: </b> $ ${element.price}
+                    <b>Price: </b> $ ${pricexml}
                   </p>
                 </div>
               </div>
             </div>`
             let elementProduct= document.getElementsByClassName("row");
-            console.log(elementProduct);
             elementProduct[0].innerHTML+=message;
         }
-        let arrProductos= xml.getElementsByTagName("product");
-        for (const element of arrProductos){
-            element.getElementsByTagName("");
-        }
-        
-
-    }
-    catch(error){
-        console.log(error);
-    }
 }
 
 loadProducts(urlJSON,urlXML);
